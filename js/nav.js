5697b420a8411eb7c5c8a885b3994ac6040c6a6d
pjvalentini <pj.valentini@gmail.com>
2018-05-09 16:44:23 -0400

    nav now fixed and links working

--- js/nav.js
+++ js/nav.js
@@ -2,18 +2,23 @@ const navToggle = document.querySelector('.menu-button');
 const nav = document.querySelector('nav');
 const containerAll = document.querySelector('.container-all');
 
+// some variables to clean up the code.
+const containerAllStyle = containerAll.style;
+const bodyCLassList = document.body.classList;
+
+
 
 navToggle.addEventListener('click', () => {
   // console.log("im clicked");
-  containerAll.style.transition = 'transform 250ms ease-in-out';
+  containerAllStyle.transition = 'transform 250ms ease-in-out';
   // adding a class using classList.add...no "." required.
   // as the "." will end up in the html as well.
   // you can replace .add with .toggle to have the class added and then removed.
-  document.body.classList.toggle('nav-is-open');
+  bodyCLassList.toggle('nav-is-open');
 });
 
 nav.addEventListener('click', () => {
   // console.log("click");
-  containerAll.style.transition = '0ms';
-  document.body.classList.remove('nav-is-open');
+  containerAllStyle.transition = '0ms';
+  bodyCLassList.remove('nav-is-open');
 });
