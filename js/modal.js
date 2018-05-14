bedf7b6626e88619077de0b2145e5de928666fb5
pjvalentini <pj.valentini@gmail.com>
2018-05-14 14:19:54 -0400

    almost complete, adding more photos and change to modal layout and functionality

--- js/modal.js
+++ js/modal.js
@@ -14,10 +14,38 @@ portfolioContainer.addEventListener('click', (e) => {
   // console.log(modal);
   const closeButton = modal.querySelector('.modal-close');
 
-  modal.classList.add('is-open');
+  const modalOpen = () => {
+     modal.classList.add('is-open');
+     modal.style.animation = "modalIn 500ms forwards";
+     // removing background scroll when the modal is open.
+     document.body.style.overflowY = "hidden";
+  };
+
+  const modalClose = () => {
+    modal.classList.remove('is-open');
+    modal.removeEventListener('animationend', modalClose);
+  };
+
   // this click will remove the is-open class.
   closeButton.addEventListener('click', () => {
-    modal.classList.remove('is-open');
+    modal.style.animation = "modalOut 500ms forwards";
+    // we need an event here so that when the animation ends modalClose function will trigger.
+    modal.addEventListener('animationend', modalClose);
+    // add a scroll to the main page when modal is closed.
+    document.body.style.overflowY = "scroll";
     // console.log("modal closes");
   });
+
+// adding the ability to exit the modal using the "esc" key (keyCode = 27)
+  document.addEventListener('keydown', (e) => {
+    // console.log(e.keyCode);
+    if (e.keyCode === 27) {
+      modal.style.animation = "modalOut 500ms forwards";
+      // we need an event here so that when the animation ends modalClose function will trigger.
+      modal.addEventListener('animationend', modalClose);
+      // add a scroll to the main page when modal is closed.
+      document.body.style.overflowY = "scroll";
+    }
+  });
+modalOpen();
 });
