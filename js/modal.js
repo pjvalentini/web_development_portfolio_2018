76fa9ff2bc5ac84ac04c5f348e0c821f1dabe47b
pjvalentini <pj.valentini@gmail.com>
2018-05-29 16:52:55 -0400

    fixed link issue and added links one more port item to go

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,6 @@ const portfolioContainer = document.querySelector('.portfolio-items');
 portfolioContainer.addEventListener('click', (e) => {
   // checking for event.
   // console.log(e);
-  e.preventDefault();
   // This will target on the specific tag you click on, portfolio-link in this case.
   const modalToggle = e.target.closest('.portfolio-link');
   console.log(modalToggle);
@@ -16,8 +15,8 @@ portfolioContainer.addEventListener('click', (e) => {
 
   const modalOpen = () => {
      modal.classList.add('is-open');
-     modal.style.animation = "modalIn 500ms forwards";
      // removing background scroll when the modal is open.
+     // modal.style.animation = "modalIn 500ms forwards"; causes an issue when scrolling...NEED TO CLICK FIRST.
      document.body.style.overflowY = "hidden";
   };
 
@@ -36,16 +35,17 @@ portfolioContainer.addEventListener('click', (e) => {
     // console.log("modal closes");
   });
 
+// THIS WAS CAUSING ISSUES WITH THE QUOTESTER MODAL --- LOOKING FOR SOLUTION
 // adding the ability to exit the modal using the "esc" key (keyCode = 27)
-  document.addEventListener('keydown', (e) => {
-    // console.log(e.keyCode);
-    if (e.keyCode === 27) {
-      modal.style.animation = "modalOut 500ms forwards";
-      // we need an event here so that when the animation ends modalClose function will trigger.
-      modal.addEventListener('animationend', modalClose);
-      // add a scroll to the main page when modal is closed.
-      document.body.style.overflowY = "scroll";
-    }
-  });
+  // document.addEventListener('keydown', (e) => {
+  //   // console.log(e.keyCode);
+  //   if (e.keyCode === 27) {
+  //     modal.style.animation = "modalOut 500ms forwards";
+  //     // we need an event here so that when the animation ends modalClose function will trigger.
+  //     modal.addEventListener('animationend', modalClose);
+  //     // add a scroll to the main page when modal is closed.
+  //     document.body.style.overflowY = "scroll";
+  //   }
+  // });
 modalOpen();
 });
